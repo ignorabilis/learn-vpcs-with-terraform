1c7c90e2225bf39596d01b46a25271eb57707b5a
ignorabilis <irina.yaroslavova@ignorabilis.com>
2023-05-11 11:21:51 +0300

    add the default subnets as well

--- defaults.tf
+++ defaults.tf
@@ -10,10 +10,26 @@ resource "aws_default_vpc" "default" {
   force_destroy = false
 }
 
-# resource "aws_default_subnet" "default_az1" {
-#   availability_zone = "us-west-2a"
+# again, importing is needed
+# for reference:
+# terraform import aws_default_subnet.default[0] subnet-d39889aa
+# terraform import aws_default_subnet.default[1] subnet-db261490
+# terraform import aws_default_subnet.default[2] subnet-37ac4f6a
+# terraform import aws_default_subnet.default[3] subnet-7bad3650
+resource "aws_default_subnet" "default" {
+  count = length(local.azs)
 
-#   tags = {
-#     Name = "Default subnet for us-west-2a"
-#   }
-# }
+  # vpc_id is computed
+  availability_zone = local.azs[count.index]
+
+  # keep it to avoid changes, the default subnets have it... by default...
+  # did I say default? 
+  timeouts {}
+
+  tags = {
+    Name = "${substr(local.azs[count.index], -1, 1)}_DO_NOT_USE_OR_DELETE_DEFAULT_TF"
+  }
+
+  # it's optional and false by default - but just to make sure it's here
+  force_destroy = false
+}
