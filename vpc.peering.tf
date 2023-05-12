6668e08737fc9fbfbaefaf55904b77f4ce767f70
ignorabilis <irina.yaroslavova@ignorabilis.com>
2023-05-12 20:18:49 +0300

    haha

---
+++ vpc.peering.tf
@@ -0,0 +1,7 @@
+# imagine the pain and suffering of overlapping CIDR ranges, misconfigured
+# routes, NACLs, security groups, firewalls...
+# yeah, that's what I thought - let's just skip
+# the principles are clear - debug mercilessly each component until it works
+# or you drop dead... to paraphrase brosciencelife's Dom Mazzetti:
+# "debug for Jesus", which means debugging until you meet your maker Jesus...
+# have fun! 
